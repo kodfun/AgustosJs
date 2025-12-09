85a38dd470dbe49e6973aead54f879a183c6a267
kodfun <kodluyorumtv@gmail.com>
2025-12-10 01:36:44 +0700

    gun 17

---
+++ Gun17/string.js
@@ -0,0 +1,21 @@
+let a = "ali";
+let b = 'veli';
+let c = '<p style="color: red">merhaba</p>';
+let d = "<p style=\"color: red\">merhaba</p>";
+let e = "BABA";
+let f = "\x42\x41\x42\x41";
+let g = "\\\""; // escape character
+let h = "\tŞİİR\nminareden atlarım\nbayramınızı kutlarım";
+let i = `   ŞİİR
+minareden atlarım
+bayramınızı kutlarım`;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g); // \"
+console.log(h);
+console.log(i);
